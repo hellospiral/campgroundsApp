94e340fab5cc0c9968e164757d63e7fa3b32872a
Matt Carlson <matthewthomascarlson@gmail.com>
2016-04-26 23:20:24 +0000

    get post route functioning in app.js, add links to templates

--- app.js
+++ app.js
@@ -6,25 +6,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+var campgrounds = [
+    {name: "Duck Creek", image: "https://pixabay.com/static/uploads/photo/2016/01/26/23/32/camp-1163419_960_720.jpg"},
+    {name: "Mountain Face", image: "https://pixabay.com/static/uploads/photo/2015/08/04/11/02/caravans-874549_960_720.jpg"},
+    {name: "Bacon Crown", image: "https://pixabay.com/static/uploads/photo/2015/05/23/00/25/utah-780108_960_720.jpg"}
+];
+
 app.get("/", function(req, res) {
    res.render("landing"); 
 });
 
 app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
-        {name: "Duck Creek", image: "https://pixabay.com/static/uploads/photo/2016/01/26/23/32/camp-1163419_960_720.jpg"},
-        {name: "Mountain Face", image: "https://pixabay.com/static/uploads/photo/2015/08/04/11/02/caravans-874549_960_720.jpg"},
-        {name: "Bacon Crown", image: "https://pixabay.com/static/uploads/photo/2015/05/23/00/25/utah-780108_960_720.jpg"}
-        ]
-    
     res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.post("/campgrounds", function(req, res) {
-    res.send("You hit the post route");
-   //get data from form and add to campgrounds array
-   
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampGround = {name: name, image: image};
+    campgrounds.push(newCampGround);
+ 
    //redirect back go campgrounds page
+   res.redirect("/campgrounds");
+   
 });
 
 app.get("/campgrounds/new", function(req, res) {
