32b0178791fff2ad330a6ac3c4ef096ffa6d70e1
Matt Carlson <matthewthomascarlson@gmail.com>
2016-04-26 23:08:45 +0000

    set up body-parser, create new.ejs

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -15,6 +18,17 @@ app.get("/campgrounds", function(req, res) {
         ]
     
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res) {
+    res.send("You hit the post route");
+   //get data from form and add to campgrounds array
+   
+   //redirect back go campgrounds page
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
