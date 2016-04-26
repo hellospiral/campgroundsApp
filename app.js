f640abce140a38357ebed936ba674ee919ef8eae
Matt Carlson <matthewthomascarlson@gmail.com>
2016-04-26 22:26:50 +0000

    get logic working in campgrounds.ejs

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.get("/campgrounds", function(req, res) {
         {name: "Duck Creek", image: "https://pixabay.com/static/uploads/photo/2016/01/26/23/32/camp-1163419_960_720.jpg"},
         {name: "Mountain Face", image: "https://pixabay.com/static/uploads/photo/2015/08/04/11/02/caravans-874549_960_720.jpg"},
         {name: "Bacon Crown", image: "https://pixabay.com/static/uploads/photo/2015/05/23/00/25/utah-780108_960_720.jpg"}
-        ];
+        ]
     
-    res.render("campgrounds");
+    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
