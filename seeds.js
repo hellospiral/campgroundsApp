e817ddb2a1ef44ba8725d06b46d0cb46570d979b
Matt Carlson <matthewthomascarlson@gmail.com>
2016-04-29 22:21:07 +0000

    add seeds.js and set it to run every time server starts

---
+++ seeds.js
@@ -0,0 +1,61 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+
+var data = [
+        {
+            name: "Highlands Mountain Valley",
+            image: "https://pixabay.com/static/uploads/photo/2015/09/02/13/14/highlands-918954_960_720.jpg",
+            description: "Epic, beautiful spot!"
+        },
+        {
+            name: "Desolation Mountain",
+            image: "https://pixabay.com/static/uploads/photo/2015/11/07/11/39/camping-1031360_960_720.jpg",
+            description: "Nothing much here except this rad view!"
+        },
+        {
+            name: "Mars",
+            image: "https://pixabay.com/static/uploads/photo/2014/10/27/01/12/camping-504661_960_720.jpg",
+            description: "Amazing rocky desert wonderland!"
+        }
+    ]
+
+function seedDb() {
+    // Remove all campgrounds
+    Campground.remove(function(err) {
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, campground){
+                if(err){
+                    console.log(err);
+                } 
+                else{
+                    console.log("added a campround!");
+                    Comment.create(
+                        {
+                            text: "I had a really weird time here actually",
+                            author: "Bucky"
+                        }, 
+                        function(err, comment) {
+                            if(err){
+                                console.log(err);
+                            } 
+                            else{
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log("created new comment!");
+                            }
+                        });
+                }
+            });
+        });
+    });
+    // add a few campgrounds
+
+    
+    // add a few comments
+}
+
+module.exports = seedDb;
