68406c8841de4486cab260f59a628a336d89d088
Matt Carlson <matthewthomascarlson@gmail.com>
2016-04-29 22:53:08 +0000

    change show route to populate foundCampground with comments array, update show template to display comments

--- app.js
+++ app.js
@@ -57,11 +57,12 @@ app.get("/campgrounds/new", function(req, res) {
 //SHOW - shows more info about one particular campground
 app.get("/campgrounds/:id", function(req, res) {
     //find the campground with provided id
-    Campground.findById(req.params.id, function(err, foundCampground) {
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
        if (err) {
            console.log(err)
        } 
        else {
+           console.log(foundCampground);
            //render show template with that campground
            res.render("show", {campground: foundCampground});
        }
