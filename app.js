0ca6144addef3f7616a47118daa9697a74c5abb4
Matt Carlson <matthewthomascarlson@gmail.com>
2016-05-02 20:06:26 +0000

    set up comment post route in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
     seedDb      = require("./seeds")
     
 seedDb();
@@ -82,6 +83,31 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
         else {
             //send campground through when we render
             res.render("comments/new", {campground: foundCampground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+    //lookup campground using id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }
+        else{
+            //create new comment
+            Comment.create(req.body.comment, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    //connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    //redirect to campground show page
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            })
         }
     });
 });
