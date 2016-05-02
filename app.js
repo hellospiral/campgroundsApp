150565fd75c4c6a08d5099838dbdff1017dea55f
Matt Carlson <matthewthomascarlson@gmail.com>
2016-05-02 20:43:29 +0000

    add public stylesheet, style show page template

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var express     = require("express"),
 seedDb();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 
--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
         {
             name: "Highlands Mountain Valley",
             image: "https://pixabay.com/static/uploads/photo/2015/09/02/13/14/highlands-918954_960_720.jpg",
-            description: "Epic, beautiful spot!"
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
         {
             name: "Desolation Mountain",
             image: "https://pixabay.com/static/uploads/photo/2015/11/07/11/39/camping-1031360_960_720.jpg",
-            description: "Nothing much here except this rad view!"
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
         {
             name: "Mars",
             image: "https://pixabay.com/static/uploads/photo/2014/10/27/01/12/camping-504661_960_720.jpg",
-            description: "Amazing rocky desert wonderland!"
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }
     ]
 
